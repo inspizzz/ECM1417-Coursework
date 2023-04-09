b27716e8b8fab8a28096ec5c381b51ef9c432dad
inspizzz <wiktor.wiejak:gmail.com>
2023-04-09 14:06:55 +0200

    started

--- components/card/card.php
+++ components/card/card.php
@@ -3,10 +3,16 @@
     <script src="./Scripts/game.js"></script>
 
     <div class="cardCont">
-        <img class="cardImage" />
+        <img class="cardImage" id=""/>
     </div>
 
     <script>
         // load a random combination of images into
+        function generateRandom() {
+            const random_skin = Math.floor(Math.random() * 3) + 1; //Bettween 1 and 10
+            const random_mouth = Math.floor(Math.random() * 6) + 1; //Bettween 1 and 10
+            const random_eyes = Math.floor(Math.random() * 6) + 1; //Bettween 1 and 10
+        }
+
     </script>
 </header>
