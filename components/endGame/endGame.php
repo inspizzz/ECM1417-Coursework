e666d2608b62424182041103736fb06b744d3750
inspizzz <wiktor.wiejak:gmail.com>
2023-04-13 20:11:05 +0200

    added end game container so that the user can decide weather to save data or start again

--- components/endGame/endGame.php
+++ components/endGame/endGame.php
@@ -1,1 +1,12 @@
-<?php
+<header id="startGame">
+    <link rel="stylesheet" href="./components/endGame/endGame.css" />
+    <script src="./Scripts/game.js"></script>
+
+    <div class="backdrop">
+
+    </div>
+
+    <div class="endGameCont">
+        <button class="saveScore" onClick=""> SaveScore </button>
+    </div>
+</header>
